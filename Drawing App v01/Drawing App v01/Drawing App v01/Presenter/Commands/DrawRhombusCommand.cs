e1bd97421639729e531f8c8963faf260872f4479
Kadar. <kadarmate99@gmail.com>
2025-03-01 11:58:07 +0100

    add Circle, add Rhombus

---
+++ Drawing App v01/Drawing App v01/Drawing App v01/Presenter/Commands/DrawRhombusCommand.cs
@@ -0,0 +1,29 @@
+﻿using Drawing_App_v01.Model.ShapeComponents;
+using Drawing_App_v01.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing_App_v01.Presenter.Commands
+{
+    internal class DrawRhombusCommand : IDrawingCommand
+    {
+        private Node _startPoint;
+
+        public DrawRhombusCommand(Node startPoint)
+        {
+            _startPoint = startPoint;
+        }
+
+        public void Execute(DrawingModel model, int x, int y)
+        {
+            if (_startPoint != null)
+            {
+                ShapeRhombus rhombus = new ShapeRhombus(_startPoint, new Node(x, y));
+                model.AddShape(rhombus);
+            }
+        }
+    }
+}
---
+++ Drawing App v01/Drawing App v01/Drawing App v01/Presenter/DrawingStates/RhombusDrawingState.cs
@@ -0,0 +1,41 @@
+﻿using Drawing_App_v01.Model;
+using Drawing_App_v01.Model.ShapeComponents;
+using Drawing_App_v01.Presenter.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drawing_App_v01.Presenter.DrawingStates
+{
+    internal class RhombusDrawingState : ShapeDrawingStateBase
+    {
+        public RhombusDrawingState() : base() { }
+
+        public override void HandleMouseDown(MainWindowPresenter presenter, DrawingModel model, int x, int y)
+        {
+            if (!_isDrawing)
+            {
+                _startPoint = new Node(x, y);
+                _isDrawing = true;
+            }
+            else
+            {
+                IDrawingCommand drawCommand = new DrawRhombusCommand(_startPoint);
+                drawCommand.Execute(model, x, y);
+                presenter.View.InvalidateCanvas();
+                _isDrawing = false;
+            }
+        }
+
+        public override void TemporaryDraw(Graphics g)
+        {
+            if (_isDrawing && _startPoint != null && _currentPoint != null)
+            {
+                ShapeRhombus rhombus = new ShapeRhombus(_startPoint, _currentPoint);
+                rhombus.Draw(g);
+            }
+        }
+    }
+}
